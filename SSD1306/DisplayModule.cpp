87e0f61563c9e3fa875e90fb025dbc7454c22c70
LogicLevel <mlm232@case.edu>
2018-03-22 18:51:15 -0400

    SSD1306 library updates

--- SSD1306/DisplayModule.cpp
+++ SSD1306/DisplayModule.cpp
@@ -2,20 +2,46 @@
 #include "DisplayModule.h"
 
 
-DisplayModule::DisplayModule(){
-
+DisplayModule::DisplayModule(SSD1306 *disp){
+  display = disp;
 }
 
 void DisplayModule::drawDisplay(){
-
+  // draw fram components here!!
   drawIndicators();
   drawIcons();
 }
 
+#define horizontal_x 48
+#define vertial_y 40
 void DisplayModule::drawIndicators(){
+  // check if poiter is not NULL
+  if (activeFrame) {
+    // Horizontal Icons
+    if (activeFrame->front != null)
+      display.drawXbm(horizontal_x, 0, indicator_horizonal_w, indicator_horizonal_h, indicator_horizonal_filled_bits);
+    else
+      display.drawXbm(horizontal_x, 59, indicator_horizonal_w, indicator_horizonal_h, indicator_horizonal_bits);
+    if (activeFrame->back != null)
+      display.drawXbm(horizontal_x, 0, indicator_horizonal_w, indicator_horizonal_h, indicator_horizonal_filled_bits);
+    else
+      display.drawXbm(horizontal_x, 59, indicator_horizonal_w, indicator_horizonal_h, indicator_horizonal_bits);
 
+    // Vertical Icons
+    if (activeFrame->right != null)
+      display.drawXbm(122, vertial_y, indicator_vertical_w, indicator_vertical_h, indicator_vertical_filled_bits);
+    else
+      display.drawXbm(122, vertial_y, indicator_vertical_w, indicator_vertical_h, indicator_vertical_bits);
+    if (activeFrame->left != null)
+      display.drawXbm(0, vertial_y, indicator_vertical_w, indicator_vertical_h, indicator_vertical_filled_bits);
+    else
+      display.drawXbm(0, vertial_y, indicator_vertical_w, indicator_vertical_h, indicator_vertical_bits);
+  }
 }
 
 void DisplayModule::drawIcons(){
-
+  if (wifiActive != NULL)
+    display.drawXbm(108, 0, wifi_w, wifi_H, wifi_bits);
+  if (blutoothActive != NULL)
+    display.drawXbm(92, 0, BT_w, BT_h, BT_bits);
 }
