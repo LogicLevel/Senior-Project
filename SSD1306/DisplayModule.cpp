b312500aa808d4e214ea461cdc5024fbaed3f2df
LogicLevel <mlm232@case.edu>
2018-03-22 17:55:03 -0400

    added DisplayModule CPP file

---
+++ SSD1306/DisplayModule.cpp
@@ -0,0 +1,21 @@
+#include "Arduino.h"
+#include "DisplayModule.h"
+
+
+DisplayModule::DisplayModule(){
+
+}
+
+void DisplayModule::drawDisplay(){
+
+  drawIndicators();
+  drawIcons();
+}
+
+void DisplayModule::drawIndicators(){
+
+}
+
+void DisplayModule::drawIcons(){
+
+}
