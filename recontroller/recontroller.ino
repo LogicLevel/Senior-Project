ee5e7e308b4d34b955048c31ebb32b8cc64d8aa1
Anno van den Akker <annovda@gmail.com>
2018-04-16 23:20:57 -0400

    Integrated fingers and confirmed double integration

--- recontroller/recontroller.ino
+++ recontroller/recontroller.ino
@@ -3,35 +3,36 @@
 #include "Adafruit_BNO055.h"
 #include "utility/imumaths.h"
 
-int loopTime = 100;  // Loop time in milliseconds
+int loopTime = 20;  // Loop time in milliseconds
 long lastLoop = 0;  // Last loop time in milliseconds
 long tempLoop = 0;  // Temp loop to reduce overhead
 
 typedef struct 
 {
   // Central hand kinematics
-  float ang_pos_hand[3];
-  float ang_vel_hand[3];
+  imu::Vector<3> grav;
+  imu::Vector<3> ang_pos_hand;
+  imu::Vector<3> ang_vel_hand;
 
-  float lin_acc_hand[3];
-  float lin_vel_hand[3];
-  float lin_pos_hand[3];
+  imu::Vector<3> lin_acc_hand;
+  imu::Vector<3> lin_vel_hand;
+  imu::Vector<3> lin_pos_hand;
 
   // Finger kinematics
-  float ang_pos_fing[3][3];
-  float ang_vel_fing[3][3];
+  imu::Vector<3> ang_pos_fing_0;
+  imu::Vector<3> ang_vel_fing_0;
 
 } r_data;
 
 TwoWire bus0 = TwoWire(1);
 //TwoWire bus1 = TwoWire(1);
 
-int r_buffer_depth = 10;
+int r_buffer_depth = 30;
 r_data* r_data_buffer; // Data buffer, n samples deep
 int data_buffer_index = 0; // Signifies the newest data index
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29, &bus0);
-Adafruit_BNO055 bno_1 = Adafruit_BNO055(55, 0x28, &bus0);
+Adafruit_BNO055 bno_0 = Adafruit_BNO055(55, 0x28, &bus0);
 
 
 // Defaults: SDA - Pin 21, SCL - Pin 22
@@ -79,7 +80,7 @@ void setup()
     }
   }
   attempts = 0;
-  while(!bno_1.begin())
+  while(!bno_0.begin())
   {
     attempts++;
     if(attempts >= max_attempts) {
@@ -100,69 +101,63 @@ void setup()
 void loop() 
 {
   if((tempLoop = millis()) - lastLoop >= loopTime) 
-  {
-    // Poll IMUs
-    sensors_event_t event; 
-    bno.getEvent(&event);
-          
+  {         
     // Update data
-    update_hand(&event);
+    update_hand();
 
     //Display data 
-    Serial.print(fromVector(r_data_buffer[data_buffer_index].lin_acc_hand) + "\n");
-    Serial.print(fromVector(r_data_buffer[data_buffer_index].ang_pos_hand) + "\n\n");
+    Serial.print(fromVector(r_data_buffer[data_buffer_index].lin_acc_hand) + ", ");
+    Serial.print(fromVector(r_data_buffer[data_buffer_index].lin_vel_hand) + "\n");
     // Serial.print("Hand Position: "); Serial.println();
     
     lastLoop = tempLoop;
   }
 }
 
-String fromVector(float* vector) {
-  String out = String(vector[0]) + ", " + String(vector[1]) + ", " + String(vector[2]);
+String fromVector(imu::Vector<3> vector) {
+  String out = String(vector.x()) + ", " + String(vector.y()) + ", " + String(vector.z());
   return out;
 }
 
-void update_hand(sensors_event_t* event)
+void update_hand()
 {
   int next_index = (data_buffer_index + 1)%r_buffer_depth; // Calculate the next index
-  memcpy(r_data_buffer[next_index].ang_pos_hand, event->orientation.v, 3*sizeof(float)); // Copy in the orientation vector
-  float x_accel = (int16_t) ((bno.read8(Adafruit_BNO055::BNO055_ACCEL_DATA_X_MSB_ADDR) << 8) | (bno.read8(Adafruit_BNO055::BNO055_ACCEL_DATA_X_LSB_ADDR)));
-  float y_accel = (int16_t) ((bno.read8(Adafruit_BNO055::BNO055_ACCEL_DATA_Y_MSB_ADDR) << 8) | (bno.read8(Adafruit_BNO055::BNO055_ACCEL_DATA_Y_LSB_ADDR)));
-  float z_accel = (int16_t) ((bno.read8(Adafruit_BNO055::BNO055_ACCEL_DATA_Z_MSB_ADDR) << 8) | (bno.read8(Adafruit_BNO055::BNO055_ACCEL_DATA_Z_LSB_ADDR)));
 
-  float x_gyro = (int16_t) ((bno.read8(Adafruit_BNO055::BNO055_GYRO_DATA_X_MSB_ADDR) << 8) | (bno.read8(Adafruit_BNO055::BNO055_GYRO_DATA_X_LSB_ADDR)));
-  float y_gyro = (int16_t) ((bno.read8(Adafruit_BNO055::BNO055_GYRO_DATA_Y_MSB_ADDR) << 8) | (bno.read8(Adafruit_BNO055::BNO055_GYRO_DATA_Y_LSB_ADDR)));
-  float z_gyro = (int16_t) ((bno.read8(Adafruit_BNO055::BNO055_GYRO_DATA_Z_MSB_ADDR) << 8) | (bno.read8(Adafruit_BNO055::BNO055_GYRO_DATA_Z_LSB_ADDR)));
-
-  r_data_buffer[next_index].lin_acc_hand[0] = x_accel;
-  r_data_buffer[next_index].lin_acc_hand[1] = y_accel;
-  r_data_buffer[next_index].lin_acc_hand[2] = z_accel;
-
-  r_data_buffer[next_index].ang_vel_hand[0] = x_gyro;
-  r_data_buffer[next_index].ang_vel_hand[1] = y_gyro;
-  r_data_buffer[next_index].ang_vel_hand[2] = z_gyro;
+  r_data_buffer[next_index].ang_pos_hand = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  r_data_buffer[next_index].ang_vel_hand = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  r_data_buffer[next_index].lin_acc_hand = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  r_data_buffer[next_index].grav = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
+  r_data_buffer[next_index].lin_acc_hand = r_data_buffer[next_index].lin_acc_hand/10.19*9.81 - r_data_buffer[next_index].grav;
+//  if(fabs(r_data_buffer[next_index].lin_acc_hand.x()) < 0.05) {
+//    r_data_buffer[next_index].lin_acc_hand = r_data_buffer[next_index].lin_acc_hand.dot(imu::Vector<3>(0,1,1));
+//  }
+//  if(fabs(r_data_buffer[next_index].lin_acc_hand.y()) < 0.05) {
+//    r_data_buffer[next_index].lin_acc_hand = r_data_buffer[next_index].lin_acc_hand.dot(imu::Vector<3>(1,0,1));
+//  }
+//  if(fabs(r_data_buffer[next_index].lin_acc_hand.z()) < 0.05) {
+//    r_data_buffer[next_index].lin_acc_hand = r_data_buffer[next_index].lin_acc_hand.dot(imu::Vector<3>(1,1,0));
+//  }
   
   // Integrate acceleration for velocity
-  for( int j = 0; j < 3; j++ )
-  {
-    r_data_buffer[next_index].lin_vel_hand[j] = 0; // Reset velocity vector
-    for( int i = 0; i < r_buffer_depth; i++ )
-    {
-      r_data_buffer[next_index].lin_vel_hand[j] += r_data_buffer[i].lin_acc_hand[j]; // Integrate
-    }
-    r_data_buffer[next_index].lin_vel_hand[j] *= (0.001 * loopTime); // Multiple by dt
-  }
 
-  // Integrate velocity for position
-  for( int j = 0; j < 3; j++ )
-  {
-    r_data_buffer[next_index].lin_pos_hand[j] = 0; // Reset position vector
-    for( int i = 0; i < r_buffer_depth; i++ )
-    {
-      r_data_buffer[next_index].lin_pos_hand[j] += r_data_buffer[i].lin_vel_hand[j]; // Integrate
+  r_data_buffer[next_index].lin_vel_hand = r_data_buffer[next_index].lin_vel_hand*0;
+  for (int i = 0; i < r_buffer_depth; i++) {
+    if (i != next_index) {
+      r_data_buffer[next_index].lin_vel_hand = r_data_buffer[next_index].lin_vel_hand + r_data_buffer[i].lin_acc_hand;
     }
-    r_data_buffer[next_index].lin_pos_hand[j] *= (0.001 * loopTime); // Multiply by dt
   }
+  r_data_buffer[next_index].lin_vel_hand = r_data_buffer[next_index].lin_vel_hand * (0.001 * loopTime);
+
+  r_data_buffer[next_index].lin_pos_hand = r_data_buffer[next_index].lin_pos_hand*0;
+  for (int i = 0; i < r_buffer_depth; i++) {
+    if (i != next_index) {
+      r_data_buffer[next_index].lin_pos_hand = r_data_buffer[next_index].lin_pos_hand + r_data_buffer[i].lin_vel_hand;
+    }
+  }
+  r_data_buffer[next_index].lin_pos_hand = r_data_buffer[next_index].lin_pos_hand * (0.001 * loopTime);
+
+  r_data_buffer[next_index].ang_pos_fing_0 = bno_0.getVector(Adafruit_BNO055::VECTOR_EULER);
+  r_data_buffer[next_index].ang_vel_fing_0 = bno_0.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
   
   data_buffer_index = next_index; // Increment the current index
 }
